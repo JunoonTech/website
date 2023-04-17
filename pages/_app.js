33851c5fb8f9aa44dc25bb8694e80585d31f266c
Arpit Gupta <arpitgupta1214@gmail.com>
2023-04-17 20:17:56 +0530

    headers

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/junoon-logo-only.png" />
+        <meta property="og:image" content="/junoon-logo-only.png" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="shortcut icon" href="/junoon-logo-only.png" />
+        <meta
+          name="description"
+          content="Welcome to the Official website of Junoon"
+        />
+        <link rel="apple-touch-icon" href="/junoon-logo-only.png" />
+        <title>Junoon</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
