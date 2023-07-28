88c25dc0c61851467428ce2a8bb912c6fe8d8b9f
Arpit Gupta <arpitgupta1214@gmail.com>
2023-07-29 05:10:28 +0530

    config update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { remotePatterns: [{ hostname: "mls.mioymreo.com" }] },
+  images: { remotePatterns: [{ hostname: "*" }] },
 };
 
 module.exports = nextConfig;
