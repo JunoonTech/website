8e49f743cc6f2addba3eb621b1d4067fe8a3c74c
Daksh-134 <117268999+Daksh-134@users.noreply.github.com>
2024-10-10 23:25:24 +0530

    .

--- app/collections/page.jsx
+++ app/collections/page.jsx
@@ -5,30 +5,31 @@ import _ from "lodash";
 
 export default async function Collections() {
   const collections = await fetchData("collection");
-  const collectionGroups = _.groupBy(
-    collections,
-    (collection) => (collection.year = dayjs(collection.date).year()),
+
+  const collectionGroups = _.groupBy(collections, (collection) =>
+    dayjs(collection.date).year()
   );
 
   const sortedCollectionGroups = Object.entries(collectionGroups).sort(
-    ([yearA], [yearB]) => yearB - yearA,
+    ([yearA], [yearB]) => yearB - yearA 
   );
 
   return (
     <main className="p-6 pt-12">
       {sortedCollectionGroups.map(([year, collections]) => {
+        const sortedCollections = collections.sort((a, b) => {
+          const dateA = dayjs(a.date);
+          const dateB = dayjs(b.date);
+          return dateB - dateA;
+        });
+
         return (
           <div key={year}>
             <div className="m-7 text-center text-3xl">{year}</div>
             <div className="flex flex-wrap items-center justify-around gap-20">
-              {collections.map((collection) => {
-                return (
-                  <CollectionCard
-                    key={collection._id}
-                    collection={collection}
-                  />
-                );
-              })}
+              {sortedCollections.map((collection) => (
+                <CollectionCard key={collection._id} collection={collection} />
+              ))}
             </div>
           </div>
         );
