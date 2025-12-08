637764b3ab31ec6e23ba09e5fe2403df47776bb4
eugene0111 <eugenesam1105@gmail.com>
2025-12-09 00:01:12 +0530

    added loader

---
+++ app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-darker">
+      <div className="size-16 animate-spin rounded-full border-4 border-dark border-t-white shadow-lg"></div>
+    </div>
+  );
+}
