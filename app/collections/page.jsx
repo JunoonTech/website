c3f5a7b0e46bc2778f0c9af399e963682d384857
eugene0111 <eugenesam1105@gmail.com>
2025-12-09 22:00:05 +0530

    tilt animation

--- app/collections/page.jsx
+++ app/collections/page.jsx
@@ -2,16 +2,17 @@ import CollectionCard from "@/components/CollectionCard";
 import fetchData from "@/lib/sanity/fetchData";
 import dayjs from "dayjs";
 import _ from "lodash";
+import TiltWrapper from "@/components/TiltWrapper";
 
 export default async function Collections() {
   const collections = await fetchData("collection");
 
   const collectionGroups = _.groupBy(collections, (collection) =>
-    dayjs(collection.date).year()
+    dayjs(collection.date).year(),
   );
 
   const sortedCollectionGroups = Object.entries(collectionGroups).sort(
-    ([yearA], [yearB]) => yearB - yearA 
+    ([yearA], [yearB]) => yearB - yearA,
   );
 
   return (
@@ -28,7 +29,9 @@ export default async function Collections() {
             <div className="m-7 text-center text-3xl">{year}</div>
             <div className="flex flex-wrap items-center justify-around gap-20">
               {sortedCollections.map((collection) => (
-                <CollectionCard key={collection._id} collection={collection} />
+                <TiltWrapper key={collection._id}>
+                  <CollectionCard collection={collection} />
+                </TiltWrapper>
               ))}
             </div>
           </div>
